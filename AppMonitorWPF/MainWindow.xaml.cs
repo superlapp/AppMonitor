306e4bb92ef19e68dcab1d0b26818f59615cccf2
superlapp <sergeylapp@gmail.com>
2016-11-23 15:20:17 +0300

    Backup

--- AppMonitorWPF/MainWindow.xaml.cs
+++ AppMonitorWPF/MainWindow.xaml.cs
@@ -1,19 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.DataVisualization.Charting;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AppMonitorWPF
 {
@@ -234,7 +225,7 @@ namespace AppMonitorWPF
         private void eventsGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
         {
             var selectedEvent = (WCF_Services.dbEvent)eventsGrid.SelectedItem;
-            long workingTimeTicks = (long)selectedEvent.WorkingTime;
+            var workingTimeTicks = (long)selectedEvent.WorkingTime;
             var workingTimeSpan = TimeSpan.FromTicks(workingTimeTicks);
         }
 
